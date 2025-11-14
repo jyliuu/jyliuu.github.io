e520e785b2c49da4229cac89c368bdb341bd120d
Jinyang Liu <jl@math.ku.dk>
2025-11-14 17:30:41 +0100

    Fix workflow

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// For a user site at https://<username>.github.io/, the site is served from the root,
+// so the base path should be "/" in both dev and production.
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/jyliuu.github.io/' : '/',
+  base: '/',
   plugins: [react()],
 })
